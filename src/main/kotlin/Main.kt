d854295a53166118e813a7c145be60137e3f21e4
JoaoLucasKnight <kirajota1@gmail.com>
2024-02-17 12:09:53 -0300

    mains

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
 package nf3
 
-import nf3.temas.ParadigmaFuncional
+
 
 fun main() {
-    val pasta = ParadigmaFuncional().
+    var f: (Int, Int) -> Int
+    f = ::subtracao
+    println(f(5, 2))
+    println(f(5, 2))
+    val funcaoLambda: (Int, Int) -> Int = { a,b -> a + b }
+    println(funcaoLambda(5, 2))
+    //==
+    f = { a,b -> a + b }
+    println(f(5, 2))
+    val funcaoAnonima3 = fun(a: Int, b: Int): Int = a + b
+    println(funcaoAnonima3(5, 2))
+
+    soma(4,5, resultado = (::println))
 }
+
+fun soma(a: Int, b: Int, resultado: (Int)-> Unit) {
+    resultado(a + b)
+}
+
+fun subtracao(a: Int, b: Int):Int = a - b
